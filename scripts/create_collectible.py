ba5b9e40a3208875e778ce537790c2cc14e841dc
Liam <liam@liams-mac-mini.home>
2021-07-14 22:39:09 +0100

    initial-commit

---
+++ scripts/create_collectible.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from brownie import SimpleCollectible, accounts, network, config
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=2-ST_BERNARD.json"
+OPENSEA_FORMAT = "https://testnets.opensea.io/assets/{}/{}"
+
+dog_metadata_dic = {
+    "PUG": "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json",
+    "SHIBA_INU": "https://ipfs.io/ipfs/QmdryoExpgEQQQgJPoruwGJyZmz6SqV4FRTX1i73CT3iXn?filename=1-SHIBA_INU.json",
+    "ST_BERNARD": "https://ipfs.io/ipfs/QmbBnUjyHHN7Ytq9xDsYF9sucZdDJLRkWz7vnZfrjMXMxs?filename=2-ST_BERNARD.json",
+}
+
+
+def main():
+    dev = accounts.add(config["wallets"]["from_key"])
+    print(network.show_active())
+    simple_collectible = SimpleCollectible[len(SimpleCollectible) - 1]
+    token_id = simple_collectible.tokenCounter()
+    transaction = simple_collectible.createCollectible(dog_metadata_dic.get("ST_BERNARD"), {"from": dev})
+    transaction.wait(1)
+    print(
+        "Awesome! You can view your NFT at {}".format(
+            OPENSEA_FORMAT.format(simple_collectible.address, token_id)
+        )
+    )
+    print('Please give up to 20 minutes, and hit the "refresh metadata" button')
